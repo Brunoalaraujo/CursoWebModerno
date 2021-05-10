bcee64d2c89f1f847df6233dca9549ed857e6ce9
Brunoalaraujo <54328102+Brunoalaraujo@users.noreply.github.com>
2021-05-10 09:59:24 -0300

    Basic notes: comments, variants and constants

---
+++ fundamentos/comentario.js
@@ -0,0 +1,12 @@
+// Comentário de uma linha
+console.log("Linha 1")
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+
+/*
+* Cmemtário de 
+* múltiplas linhas
+*/
---
+++ fundamentos/variaveisEconstantes.js
@@ -0,0 +1,5 @@
+var a = 3 // "a" é o identificador da variável 
+let b = 4 //Criar variáveis usando let
+
+const c = 5
+c = 500 //Não podemos alterar valores de constantes
