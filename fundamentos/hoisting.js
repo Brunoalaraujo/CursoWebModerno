defbd5cded6a9e4f6fdfc25d43aa09242783af01
Brunoalaraujo <54328102+Brunoalaraujo@users.noreply.github.com>
2021-05-14 16:08:50 -0300

    Basic notes: hoisting, objects2 and name/value

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,12 @@
+console.log("a =", a)
+var a = 2
+console.log("a =", a)
+
+// Hoisitng é o içamento da variável para o topo do código, utilizando a palavara var
+// Não utilizar 
+
+console.log("b =", b)
+let b = 2
+console.log("b =", b)
+
+// Hoisting não se aplica ao let
